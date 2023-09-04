cd11d7ee2d202bc96692b8aa3bb429220c327bac
Kush Mirchandani <kmirchandani2006@gmail.com>
2023-09-04 16:44:24 -0400

    update again

--- Deuce/DeuceApp.swift
+++ Deuce/DeuceApp.swift
@@ -2,7 +2,37 @@
 //  DeuceApp.swift
 //  Deuce
 //
-//  Created by Kush Mirchandani on 9/4/23.
+//  Created by Kush Mirchandani on 7/17/23.
 //
+import Firebase
+import FirebaseCore
+import SwiftUI
 
-import Foundation
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
+
+@main
+struct DeuceApp: App {
+    let persistenceController = PersistenceController.shared
+    let tabBarColor = Color("#E4E4E3")
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    init() {
+        // Set tab bar appearance
+        UITabBar.appearance().backgroundColor = UIColor(tabBarColor)
+        UITabBar.appearance().unselectedItemTintColor = .gray
+
+      }
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
